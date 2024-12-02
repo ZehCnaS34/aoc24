81b304dc67b2f3a39f8e3b8bfa407c78a23c0dbb
Alexander Sanchez <alex@violet.city>
2024-12-01 23:42:34 -0800

    day 2 part 2

--- src/day_2.clj
+++ src/day_2.clj
@@ -10,9 +10,24 @@
   [coll]
   (and
     (every? #(<= 1 % 3) (map (comp abs -) coll (rest coll)))
-    (= 1 (count (into #{} (map (comp pos-int? -) coll (rest coll)))))))
+    (= 1 (count (into #{} (map (comp pos-int? -) coll (rest coll)))))
+    coll))
+
+(defn safe-after-edit?
+  ([coll]
+   (some
+     safe?
+     (for [i (range (count coll))
+           :let [left (take i coll)
+                 right (drop (inc i) coll)]]
+       (concat left right)))))
 
 (def part-1
   (as-> input $
         (filter safe? $)
+        (count $)))
+
+(def part-2
+  (as-> input $
+        (filter safe-after-edit? $)
         (count $)))
