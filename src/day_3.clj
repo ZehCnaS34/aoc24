2afa646e439251cf04a653db86efa7e5e3c0fbfe
Alexander Sanchez <alex@violet.city>
2024-12-03 00:04:55 -0800

    day 3 part 2

--- src/day_3.clj
+++ src/day_3.clj
@@ -1,11 +1,11 @@
 (ns day-3
-  (:use [prelude])
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:use [prelude]))
 
 (def input
   (->> (binding [*test* false]
-        (slurp-problem 3 1))
-      (str/join "")))
+         (slurp-problem 3 1))
+       (str/join "")))
 
 (def part-1
   (let [m (re-matcher #"mul\((\d{1,3}),(\d{1,3})\)" input)]
@@ -14,3 +14,29 @@
          (take-while some?)
          (map (comp #(apply * %) #(map parse-long %) rest))
          (reduce +))))
+
+(defn compute-part-2
+  ([input]
+   (compute-part-2 input true))
+  ([input enabled?]
+   (if (empty? input)
+     0
+     (case (first input)
+       :do (compute-part-2 (rest input) true)
+       :don't (compute-part-2 (rest input) false)
+       (if-not enabled?
+         (compute-part-2 (rest input) enabled?)
+         (+ (first input) (compute-part-2 (rest input) enabled?)))))))
+
+(def part-2
+  (let [m (re-matcher #"mul\((\d{1,3}),(\d{1,3})\)|(do|don't)\(\)" input)]
+    (->> #(re-find m)
+         (repeatedly)
+         (take-while some?)
+         (map
+           (fn [[_ & match]]
+             (let [match (remove nil? match)]
+               (case (count match)
+                 2 (apply * (map parse-long match))
+                 (-> match first keyword )))))
+         (compute-part-2))))
