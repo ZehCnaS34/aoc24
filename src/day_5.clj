3cbb3e0edffabe80594e652c29bf1602c0b7d389
Alexander Sanchez <alex@violet.city>
2024-12-05 23:00:51 -0800

    day 5 part 2

--- src/day_5.clj
+++ src/day_5.clj
@@ -5,7 +5,7 @@
   (:use [prelude]))
 
 (def input
-  (binding [*test* true]
+  (binding [*test* false]
     (as-> (slurp-problem 5 1) $
           (str/join "\n" $)
           (str/split $ #"\n\n")
@@ -44,3 +44,41 @@
        (reduce +)))
 
 (t/is (= 4462 part-1))
+
+(defn topological-sort
+  [before nodes]
+  (let [cleanup-set #(set/difference % (set/difference % (into #{} nodes)))
+        before (->> (select-keys before nodes)
+                    (map #(vector (key %) (cleanup-set (val %))))
+                    (into {}))
+        output (atom [])
+        temp? (atom #{})
+        perm? (atom #{})]
+    (letfn [(visit [n]
+              (cond
+                (@perm? n) nil
+                (@temp? n) nil
+                :else
+                (do
+                  (swap! temp? conj n)
+                  (doseq [m (before n)]
+                    (visit m))
+                  (swap! perm? conj n)
+                  (swap! output conj n))))]
+      (doseq [node nodes
+              :when (not (@perm? node))]
+        (visit node))
+      @output)))
+
+(def part-2-xf
+  (let [{:keys [before]} (:ordering input)]
+    (comp
+      (remove valid-update?)
+      (map (partial topological-sort before))
+      (map valid-update?))))
+
+(def part-2
+  (transduce part-2-xf + 0 (:updates input)))
+
+(t/is (= 6767 part-2))
+
